2a31a09db1d6053a0aba063a97096b86df917f7c
xxcolfang <109650612+xxcolfang@users.noreply.github.com>
2022-11-23 16:00:14 +0800

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,11 +1,9 @@
 import os
 from collections import namedtuple
 import yaml
-
 # getting content root directory
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
-
 OUTPUT_DIR = os.path.join(parent, "activities")
 GPX_FOLDER = os.path.join(parent, "GPX_OUT")
 TCX_FOLDER = os.path.join(parent, "TCX_OUT")
@@ -16,24 +14,18 @@ FOLDER_DICT = {
 }
 SQL_FILE = os.path.join(parent, "scripts", "data.db")
 JSON_FILE = os.path.join(parent, "src", "static", "activities.json")
-
 # TODO: Move into nike_sync
 BASE_URL = "https://api.nike.com/sport/v3/me"
 TOKEN_REFRESH_URL = "https://unite.nike.com/tokenRefresh"
 NIKE_CLIENT_ID = "HlHa2Cje3ctlaOqnxvgZXNaAs7T9nAuH"
 BASE_TIMEZONE = "Asia/Shanghai"
-
-
 start_point = namedtuple("start_point", "lat lon")
 run_map = namedtuple("polyline", "summary_polyline")
-
 try:
     with open("config.yaml") as f:
         _config = yaml.safe_load(f)
 except:
     _config = {}
-
-
 def config(*keys):
     def safeget(dct, *keys):
         for key in keys:
@@ -42,10 +34,7 @@ def config(*keys):
             except KeyError:
                 return None
         return dct
-
     return safeget(_config, *keys)
-
-
 # add more type here
 TYPE_DICT = {
     "running": "Run",
@@ -67,6 +56,7 @@ TYPE_DICT = {
     "kayaking": "Kayaking",
 }
 
+MAPPING_TYPE = ["Hike", "Ride", "VirtualRide", "Rowing", "Run", "Swim", "RoadTrip"]
 MAPPING_TYPE = [
     "Hike",
     "Ride",
